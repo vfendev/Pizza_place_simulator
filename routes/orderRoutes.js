9cfae1b35fe606721f38d97b651e960c61cf2371
vfendev <velimir.patrnogic@gmail.com>
2021-05-13 18:44:53 +0200

    checking orders by id

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -7,7 +7,7 @@ router.post('/order', async (req, res) => {
     const order = new Tasks(req.body)
     try {
         await order.save()
-        res.status(201).send(order)
+        res.status(202).send(order)
     } catch (e) {
         res.status(400).send(e)
     }
@@ -22,5 +22,18 @@ router.get('/orders', async (req, res) => {
         res.status(500).send()
     }
 })
+
+// Checking unique orders by id
+router.get('/orders/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const order = await Tasks.findById(_id)
+        res.status(202).send(order)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+
 
 module.exports = router
